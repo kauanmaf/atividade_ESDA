bb7a86818ebb884e8ca166d427ec59dbdc0b1882
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2024-03-25 08:12:09 -0300

    fatorial iterativo

--- matematica.cpp
+++ matematica.cpp
@@ -5,6 +5,20 @@ using std::cout;
 using std:: endl;
 using std::string;
 
+int pow(int, int);
+int fatorial_iterativo(int );
+
+int main() {
+    // Primeira função
+    int iresult1 = pow(2, 3); 
+    std::cout << "2^3 = " << iresult1 << std::endl;
+
+    // Segunda função
+    int iresult2 = fatorial_iterativo(5); 
+    std::cout << "5! = " << iresult2 << std::endl;
+    return 0;
+}
+
 int pow(int iBase, int iExpoente) {
     int iResposta = 1;
     
@@ -14,13 +28,12 @@ int pow(int iBase, int iExpoente) {
     return iResposta;
 }
 
-int main() {
-    int iresult = pow(2, 3);
-    std::cout << iresult;
-    return 0;
+int fatorial_iterativo(int iFator) {
+    int iResult = 1;
+    for (int iCount = iFator; iCount > 0; iCount--) {
+        iResult *= iCount;
+    }
+    return iResult; 
 }
-// int fatorial_iterativo(int iFator){
-
-// }
 
 
